e92891a1de08d5fa95dcfa63145b2a3733197162
Rohith <menonrohith94@gmail.com>
2021-04-11 14:18:51 +0530

    Mongo DB dummy data file uyploaded.

---
+++ backend/documents/new-feedback.js
@@ -0,0 +1,277 @@
+[
+    {
+        "_id": ObjectId("5be6c2e5ab06d25a566794aa"),
+        "training": "jQuery",
+        "data": {
+            "Infrastructure": 5,
+            "Material": 8,
+            "Trainer_Knowledge": 7,
+            "Hardware_Software": 7,
+            "Food": 7
+        }
+    },
+    {
+        "_id": ObjectId("5be6c2e5ab06d25a566794ab"),
+        "training": "jQuery",
+        "data": {
+            "Infrastructure": 5,
+            "Material": 8,
+            "Trainer_Knowledge": 7,
+            "Hardware_Software": 7,
+            "Food": 7
+        }
+    },
+    {
+        "_id": ObjectId("5be6c2e5ab06d25a566794ac"),
+        "training": "jQuery",
+        "data": {
+            "Infrastructure": 5,
+            "Material": 8,
+            "Trainer_Knowledge": 7,
+            "Hardware_Software": 7,
+            "Food": 7
+        }
+    },
+    {
+        "_id": ObjectId("5be6c2e5ab06d25a566794ad"),
+        "training": "jQuery",
+        "data": {
+            "Infrastructure": 5,
+            "Material": 8,
+            "Trainer_Knowledge": 7,
+            "Hardware_Software": 7,
+            "Food": 7
+        }
+    },
+    {
+        "_id": ObjectId("5be6c2e5ab06d25a566794ae"),
+        "training": "jQuery",
+        "data": {
+            "Infrastructure": 5,
+            "Material": 8,
+            "Trainer_Knowledge": 7,
+            "Hardware_Software": 7,
+            "Food": 7
+        }
+    },
+    {
+        "_id": ObjectId("5be6cbcdab06d25a566794af"),
+        "training": "Angular JS 1.x",
+        "data": {
+            "Infrastructure": 3,
+            "Material": 8,
+            "Trainer_Knowledge": 8,
+            "Hardware_Software": 8,
+            "Food": 8
+        }
+    },
+    {
+        "_id": ObjectId("5be6cbcdab06d25a566794b0"),
+        "training": "Angular JS 1.x",
+        "data": {
+            "Infrastructure": 3,
+            "Material": 8,
+            "Trainer_Knowledge": 8,
+            "Hardware_Software": 8,
+            "Food": 8
+        }
+    },
+    {
+        "_id": ObjectId("5be6cbcdab06d25a566794b1"),
+        "training": "Angular JS 1.x",
+        "data": {
+            "Infrastructure": 3,
+            "Material": 8,
+            "Trainer_Knowledge": 8,
+            "Hardware_Software": 8,
+            "Food": 8
+        }
+    },
+    {
+        "_id": ObjectId("5be6cbcdab06d25a566794b2"),
+        "training": "Angular JS 1.x",
+        "data": {
+            "Infrastructure": 3,
+            "Material": 8,
+            "Trainer_Knowledge": 8,
+            "Hardware_Software": 8,
+            "Food": 8
+        }
+    },
+    {
+        "_id": ObjectId("5be6cbcdab06d25a566794b3"),
+        "training": "Angular JS 1.x",
+        "data": {
+            "Infrastructure": 3,
+            "Material": 8,
+            "Trainer_Knowledge": 8,
+            "Hardware_Software": 8,
+            "Food": 8
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdd9f"),
+        "training": "Angular 6",
+        "data": {
+            "Infrastructure": 6,
+            "Material": 7,
+            "Trainer_Knowledge": 8,
+            "Hardware_Software": 9,
+            "Food": 10
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda0"),
+        "training": "Angular 6",
+        "data": {
+            "Infrastructure": 9,
+            "Material": 6,
+            "Trainer_Knowledge": 7,
+            "Hardware_Software": 7,
+            "Food": 4
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda1"),
+        "training": "Angular 6",
+        "data": {
+            "Infrastructure": 9,
+            "Material": 9,
+            "Trainer_Knowledge": 9,
+            "Hardware_Software": 9,
+            "Food": 9
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda2"),
+        "training": "Angular 6",
+        "data": {
+            "Infrastructure": 6,
+            "Material": 4,
+            "Trainer_Knowledge": 3,
+            "Hardware_Software": 3,
+            "Food": 3
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda3"),
+        "training": "Angular 6",
+        "data": {
+            "Infrastructure": 5,
+            "Material": 5,
+            "Trainer_Knowledge": 5,
+            "Hardware_Software": 5,
+            "Food": 5
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda4"),
+        "training": "Bootstrap 4.x",
+        "data": {
+            "Infrastructure": 9,
+            "Material": 9,
+            "Trainer_Knowledge": 9,
+            "Hardware_Software": 9,
+            "Food": 9
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda5"),
+        "training": "Bootstrap 4.x",
+        "data": {
+            "Infrastructure": 9,
+            "Material": 6,
+            "Trainer_Knowledge": 7,
+            "Hardware_Software": 7,
+            "Food": 4
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda6"),
+        "training": "Bootstrap 4.x",
+        "data": {
+            "Infrastructure": 9,
+            "Material": 9,
+            "Trainer_Knowledge": 9,
+            "Hardware_Software": 9,
+            "Food": 9
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda7"),
+        "training": "Bootstrap 4.x",
+        "data": {
+            "Infrastructure": 6,
+            "Material": 4,
+            "Trainer_Knowledge": 3,
+            "Hardware_Software": 3,
+            "Food": 3
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda8"),
+        "training": "Bootstrap 4.x",
+        "data": {
+            "Infrastructure": 5,
+            "Material": 5,
+            "Trainer_Knowledge": 5,
+            "Hardware_Software": 5,
+            "Food": 5
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebdda9"),
+        "training": "Node JS",
+        "data": {
+            "Infrastructure": 9,
+            "Material": 9,
+            "Trainer_Knowledge": 9,
+            "Hardware_Software": 9,
+            "Food": 9
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebddaa"),
+        "training": "Node JS",
+        "data": {
+            "Infrastructure": 9,
+            "Material": 6,
+            "Trainer_Knowledge": 7,
+            "Hardware_Software": 7,
+            "Food": 4
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebddab"),
+        "training": "Node JS",
+        "data": {
+            "Infrastructure": 9,
+            "Material": 9,
+            "Trainer_Knowledge": 9,
+            "Hardware_Software": 9,
+            "Food": 9
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebddac"),
+        "training": "Node JS",
+        "data": {
+            "Infrastructure": 6,
+            "Material": 4,
+            "Trainer_Knowledge": 3,
+            "Hardware_Software": 3,
+            "Food": 3
+        }
+    },
+    {
+        "_id": ObjectId("5bed719acf217f6babebddad"),
+        "training": "Node JS",
+        "data": {
+            "Infrastructure": 5,
+            "Material": 5,
+            "Trainer_Knowledge": 5,
+            "Hardware_Software": 5,
+            "Food": 5
+        }
+    }
+]
